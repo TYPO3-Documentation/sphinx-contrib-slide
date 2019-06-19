322c1cf2860ed6cc658fd91d27f93b28139ffb31
Martin Bless <martin.bless@mbless.de>
2019-06-19 12:15:58 +0200

    Remove development code for Google docs and slideshare

--- sphinxcontrib/slide.py
+++ sphinxcontrib/slide.py
@@ -153,37 +153,3 @@ def setup(app):
                  latex=(latex_visit_slide_node, depart_slide_node))
     app.add_directive('slide', SlideDirective)
 
-
-if 0 and __name__ == "__main__" and "probing slideshare":
-    response_example = {
-        "author_name": "TRITUM",
-        "author_url": "https://www.slideshare.net/TRITUM",
-        "conversion_version": 2,
-        "height": 355,
-        "html": "<iframe src=\"https://www.slideshare.net/slideshow/embed_code/key/28iL36exB50UkP\" width=\"427\" height=\"356\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" style=\"border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;\" allowfullscreen> </iframe> <div style=\"margin-bottom:5px\"> <strong> <a href=\"https://www.slideshare.net/TRITUM/whats-new-in-typo3-v9-lts\" title=\"What&#x27;s new in TYPO3 v9 LTS\" target=\"_blank\">What&#x27;s new in TYPO3 v9 LTS</a> </strong> from <strong><a href=\"https://www.slideshare.net/TRITUM\" target=\"_blank\">TRITUM</a></strong> </div>\n\n",
-        "provider_name": "SlideShare",
-        "provider_url": "https://www.slideshare.net/",
-        "slide_image_baseurl": "//image.slidesharecdn.com/typo3v9lts-181007112714/95/slide-",
-        "slide_image_baseurl_suffix": "-1024.jpg",
-        "slideshow_id": 118556992,
-        "thumbnail": "//cdn.slidesharecdn.com/ss_thumbnails/typo3v9lts-181007112714-thumbnail.jpg?cb=1538911762",
-        "thumbnail_height": 128,
-        "thumbnail_url": "https://cdn.slidesharecdn.com/ss_thumbnails/typo3v9lts-181007112714-thumbnail.jpg?cb=1538911762",
-        "thumbnail_width": 170,
-        "title": "What's new in TYPO3 v9 LTS",
-        "total_slides": 52,
-        "type": "rich",
-        "version": "1.0",
-        "version_no": "1538911762",
-        "width": 425
-    }
-    url = 'http://www.slideshare.net/TRITUM/whats-new-in-typo3-v9-lts'
-    get_slide_options_for_slideshare(url)
-
-if 0 and __name__ == "__main__" and "probing google":
-    embed_code = """<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQEjSEE6PtyibpB8ZhDHAEhUwkXqHfbTdCHHehJYrElzTTVy2MS7dh-Pvyz1qwVypKfOXI3TE0QUxk4/embed?start=false&loop=false&delayms=3000" frameborder="0" width="480" height="299" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>"""
-    response_example = {
-    }
-    url = ('https://docs.google.com/presentation/d/1FOVjpJIJrHC4Wly9rsGelDdfXe4bgamLOJh1GlVx2Tk/present')
-    get_slide_options_for_googledocs(url)
-
